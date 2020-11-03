c06a5bffc250457d4753bd73d6b8de6c4ff7348c
Kogonidze <kottok1998@gmail.com>
2020-11-03 17:35:23 +0300

    binded component Product

--- ishop2/Components/BunchOfProducts.jsx
+++ ishop2/Components/BunchOfProducts.jsx
@@ -19,31 +19,38 @@ var BunchOfProducts = React.createClass({
     }, 
 
     render: function() {
-        var productsCode = [];
-        var ColumnNames = 
-                React.DOM.div({key: 0, className: 'Row'},
-                React.DOM.div({className: 'Cell ColumnNames'}, 'Наименование товара'),
-                React.DOM.div({className: 'Cell ColumnNames'}, 'Цена (в BYN)'),
-                React.DOM.div({className: 'Cell ColumnNames'}, 'Остаток'),
-                React.DOM.div({className: 'Cell ColumnNames ToCenter'}, 'Фото'),
-                );
+        var productsCode = this.props.products.map( product => 
+            React.createElement(Product, {key: product.barcode, 
+                                name: product.name, 
+                                price: product.price, 
+                                count: product.count,
+                                photo: product.photo,
+                            }));
+        // var productsCode = [];
+        // var ColumnNames = 
+        //         React.DOM.div({key: 0, className: 'Row'},
+        //         React.DOM.div({className: 'Cell ColumnNames'}, 'Наименование товара'),
+        //         React.DOM.div({className: 'Cell ColumnNames'}, 'Цена (в BYN)'),
+        //         React.DOM.div({className: 'Cell ColumnNames'}, 'Остаток'),
+        //         React.DOM.div({className: 'Cell ColumnNames ToCenter'}, 'Фото'),
+        //         );
 
-        productsCode.push(ColumnNames);
+        // productsCode.push(ColumnNames);
 
-        allProducts = this.props.products;
-        allProducts.forEach(function(item, i, allProducts) {
-            var product = allProducts[i];
+        // allProducts = this.props.products;
+        // allProducts.forEach(function(item, i, allProducts) {
+        //     var product = allProducts[i];
 
-            var productCode = 
-                React.DOM.div({key: product.barcode, className: 'Row'},
-                React.DOM.div({className: 'Cell Text'}, product.name),
-                React.DOM.div({className: 'Cell Text ToCenter'}, product.price),
-                React.DOM.div({className: 'Cell Text ToCenter'}, product.count),
-                React.DOM.img({src: product.photo, alt:'Product Sample', className: 'Cell Image'}),
-                );
+        //     var productCode = 
+        //         React.DOM.div({key: product.barcode, className: 'Row'},
+        //         React.DOM.div({className: 'Cell Text'}, product.name),
+        //         React.DOM.div({className: 'Cell Text ToCenter'}, product.price),
+        //         React.DOM.div({className: 'Cell Text ToCenter'}, product.count),
+        //         React.DOM.img({src: product.photo, alt:'Product Sample', className: 'Cell Image'}),
+        //         );
 
-            productsCode.push(productCode);
-        });
+        //     productsCode.push(productCode);
+        // });
 
         return React.DOM.div({className: 'BunchOfProducts'},
         React.createElement(Shop, {shop: this.props.shop}),
--- ishop2/Components/Product.jsx
+++ ishop2/Components/Product.jsx
@@ -3,13 +3,16 @@ var Product = React.createClass({
 
     propTypes: {
         name: React.PropTypes.string.isRequired,
-        barcode: React.PropTypes.number.isRequired,
         price: React.PropTypes.number.isRequired,
         count: React.PropTypes.number.isRequired,
         photo: React.PropTypes.string.isRequired,
     },
 
     render: function() {
-
+        return React.DOM.div({className: 'Row'},
+                React.DOM.div({className: 'Cell Text'}, this.props.name),
+                React.DOM.div({className: 'Cell Text ToCenter'}, this.props.price),
+                React.DOM.div({className: 'Cell Text ToCenter'}, this.props.count),
+                React.DOM.img({src: this.props.photo, alt:'Product Sample', className: 'Cell Image'}));
     },
 })
