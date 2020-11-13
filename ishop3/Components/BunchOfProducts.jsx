4f0e79ea614a648c3163364f73c28e0cba8b9ea8
Kogonidze <kottok1998@gmail.com>
2020-11-13 20:08:01 +0300

    added npm

---
+++ ishop3/Components/BunchOfProducts.jsx
@@ -0,0 +1,67 @@
+var BunchOfProducts = React.createClass({
+    displayName: 'BunchOfProducts',
+
+    propTypes: {
+        shop: React.PropTypes.string.isRequired,
+        product: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                name: React.PropTypes.string.isRequired,
+                barcode: React.PropTypes.number.isRequired,
+                price: React.PropTypes.number.isRequired,
+                count: React.PropTypes.number.isRequired,
+                photo: React.PropTypes.string.isRequired,
+            })
+        ),
+    },
+    
+    getDefaultProps: function() {
+        return { shop: "Безымянный"}
+    }, 
+
+    getInitialState: function() {
+        return {
+            productToDelete: [],
+            selectedProduct: null,
+        };
+    },
+
+    cbSelectRow: function(code) {
+        this.setState({selectedProduct: code});
+    },
+
+    cbDeleteRow: function(code) {
+        var deletedRows = this.state.productToDelete.slice();
+        deletedRows.push(code);
+        this.setState({productToDelete: deletedRows});
+    },
+
+    render: function() {
+        var productsCode = this.props.products.map( product => 
+            React.createElement(Product, {key: product.barcode, 
+                                name: product.name, 
+                                price: product.price, 
+                                count: product.count,
+                                photo: product.photo,
+                                barcode: product.barcode,
+                                cbSelectRow: this.cbSelectRow,
+                                cbDeleteRow: this.cbDeleteRow,
+                                selectedProduct: this.state.selectedProduct,
+                                productToDelete: this.state.productToDelete,
+                            }));
+
+        return React.DOM.div({className: 'BunchOfProducts'},
+        React.createElement(Shop, {shop: this.props.shop}),
+        React.DOM.table({className: 'Table'},
+            React.DOM.tbody(null, 
+                React.DOM.tr({className: 'Row'}, 
+                    React.DOM.th({className: 'Cell ColumnNames ToCenter'}, 'Наименование товара'),
+                    React.DOM.th({className: 'Cell ColumnNames ToCenter'}, 'Цена (в BYN)'),
+                    React.DOM.th({className: 'Cell ColumnNames ToCenter'}, 'Остаток'),
+                    React.DOM.th({className: 'Cell ColumnNames ToCenter'}, 'Фото'),
+                    React.DOM.th({className: 'Cell ColumnNames ToCenter'}, "Удаление"),
+                ),
+                ({className: 'Row'}, productsCode)
+        )),
+        );
+    },
+});
