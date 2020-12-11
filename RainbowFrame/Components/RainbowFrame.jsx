41df559cfb832c2f108790d6c6d0e14215dbe5a3
kogonidze <kottok1998@gmail.com>
2020-12-11 16:03:30 +0300

    changed function to arrow function

--- RainbowFrame/Components/RainbowFrame.jsx
+++ RainbowFrame/Components/RainbowFrame.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function RainbowFrame({colors, children}) {
-    return (<div style={{border:"solid 5px "+colors.pop(),padding:"10px", textAlign:"center", fontWeight:"bold"}}> 
-    {colors.length > 0 && <RainbowFrame colors={colors} children={children} /> }
-    {colors.length == 0 && children}
+const RainbowFrame = props => {
+    return (<div style={{border:"solid 5px "+props.colors.pop(),padding:"10px", textAlign:"center", fontWeight:"bold"}}> 
+    {props.colors.length > 0 && <RainbowFrame colors={props.colors} children={props.children} /> }
+    {props.colors.length == 0 && props.children}
     </div>
     )};
 
 RainbowFrame.propTypes = {
     colors: PropTypes.arrayOf(PropTypes.string),
     children: PropTypes.string
-}
+};
+
+export default RainbowFrame;
