4ed6d8ab0f7ae307a702d2cdb2983202eb6692fc
kogonidze <kottok1998@gmail.com>
2020-12-13 12:59:25 +0300

    initial commit

---
+++ RainbowFrameHOC/Components/MainBlock.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MainBlock extends React.Component {
+    render() {
+        let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+
+        return(null);
+    }
+}
+
+export default MainBlock;
