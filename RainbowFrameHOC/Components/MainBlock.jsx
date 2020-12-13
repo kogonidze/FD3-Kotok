6d64fde55c6cdfdb8196f753e6ecb245c1587ab4
kogonidze <kottok1998@gmail.com>
2020-12-13 14:48:04 +0300

    done

--- RainbowFrameHOC/Components/MainBlock.jsx
+++ RainbowFrameHOC/Components/MainBlock.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import DoubleButton from './DoubleButton.jsx';
+import {withRainbowFrame} from './WithRainbowFrame.jsx';
 
 class MainBlock extends React.Component {
     render() {
         let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+        let FramedDoubleButton = withRainbowFrame(colors)(DoubleButton);
 
-        return(null);
+        return(<Fragment>
+            <DoubleButton caption1="однажды" caption2="пору" cbPressed={ num => alert(num) }> в студеную зимнюю пору </DoubleButton>
+            <br/>
+            <br/>
+            <FramedDoubleButton caption1="я из лесу" caption2="мороз" cbPressed={ num => alert(num) }>вышел, был сильный</FramedDoubleButton>
+        </Fragment>
+        );
     }
 }
 
