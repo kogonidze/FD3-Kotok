83e9c47e5a38e3617a43a9441b7410ae9ab07f01
kogonidze <kottok1998@gmail.com>
2020-12-17 15:48:08 +0300

    initial commit

---
+++ MobileWithTests/__tests__/component-test.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import renderer from 'react-test-renderer';
+import MobileCompany from '../components/MobileCompany';
+
+import ExampleComponent from '../components/MobileCompany';
+
+test('работа ExampleComponent', () => {
+
+  const component = renderer.create(
+    <MobileCompany />
+  );
+
+  let componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+    
+});
