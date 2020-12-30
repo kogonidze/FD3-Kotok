0b132e0f43f75bdcd291db67310a9f9866c4c9e4
kogonidze <kottok1998@gmail.com>
2020-12-30 17:01:33 +0300

    added some distinction between classes Apple and Tomato

--- Scales2/app.ts
+++ Scales2/app.ts
@@ -1,68 +1,68 @@
 interface IScalable {
-    getScale() : number;
-    getName() : string;
+  getScale(): number;
+  getName(): string;
 }
 
 class Scales {
-    products: IScalable[] = [];
+  products: IScalable[] = [];
 
-    add(newProduct: IScalable) : void{
-        this.products.push(newProduct);
-    }
+  add(newProduct: IScalable): void {
+    this.products.push(newProduct);
+  }
 
-    getNameList() : string[] {
-        var nameList: string[] = [];
+  getNameList(): string[] {
+    var nameList: string[] = [];
 
-        this.products.forEach((product : IScalable) => {
-            nameList.push(product.getName());
-        });
+    this.products.forEach((product: IScalable) => {
+      nameList.push(product.getName());
+    });
 
-        return nameList;
-    }
+    return nameList;
+  }
 
-    getSumScale() : number {
-        var sumScale: number = 0;
+  getSumScale(): number {
+    var sumScale: number = 0;
 
-        this.products.forEach((product : IScalable) => {
-            var scale : number = product.getScale();
-            sumScale += scale;
-        });
+    this.products.forEach((product: IScalable) => {
+      var scale: number = product.getScale();
+      sumScale += scale;
+    });
 
-        return sumScale;
-    }
+    return sumScale;
+  }
 }
 
 class Apple implements IScalable {
-    constructor(private name: string, private scale: number) {}
+  constructor(private name: string, private scale: number) {}
 
-    getName(): string {
-        return this.name;
-    }
+  getName(): string {
+    return `Apple ${this.name}`;
+  }
 
-    getScale(): number {
-        return this.scale;
-    }
+  getScale(): number {
+    return this.scale;
+  }
 }
 
 class Tomato implements IScalable {
-    constructor(private name: string, private scale: number) {}
+  constructor(private name: string, private scale: number) {}
 
-    getName(): string {
-        return this.name;
-    }
+  getName(): string {
+    return `Tomato ${this.name}`;
+  }
 
-    getScale(): number {
-        return this.scale;
-    }
+  getScale(): number {
+    return this.scale;
+  }
 }
 
-let scale : Scales = new Scales();
+let scale: Scales = new Scales();
 
-let redApple : Apple = new Apple("red", 500);
-let greenApple : Apple = new Apple("green", 700);
+let redApple: Apple = new Apple("red", 500);
+let greenApple: Apple = new Apple("green", 700);
 
-let smallTomato : Tomato = new Tomato("small", 200);
-let bigTomato : Tomato = new Tomato("big", 600);
+let smallTomato: Tomato = new Tomato("small", 200);
+let bigTomato: Tomato = new Tomato("big", 600);
 
 scale.add(redApple);
 scale.add(greenApple);
